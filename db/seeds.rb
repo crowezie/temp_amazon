0c58cae4ef4cef51eaa43d1763c2199a6f1d18d6
Brett Crowe <crowe706@gmail.com>
2018-10-16 11:40:03 -0700

    Product model

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,18 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+# Make sure to clear old data out of your db when you run rails db:seed
+Product.destroy_all
+
+# Now go ahead and create new data
+1000.times do
+  Product.create({
+    title: Faker::Hacker.noun,
+    description: Faker::Hacker.say_something_smart,
+    price: Faker::Commerce.price
+  })
+end
+
+# It's nice to print object counts to make sure it created what you expected.
+puts "Created #{Product.count} products"
