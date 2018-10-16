fec75972154d9328ab3c6b4a1796b69d13f38514
Brett Crowe <crowe706@gmail.com>
2018-10-16 09:41:46 -0700

    Build a contact us page

--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,6 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root 'welcome#index'
   get('/about', { to: 'welcome#about', as: :about })
+  get('/contact_us', { to: 'contacts#index', as: :contact })
+  post('/contact_us', { to: 'contacts#create' })
 end
