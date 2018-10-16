29458d2b9f47c71d6d08e9d334d08ce07a10d0d5
Brett Crowe <crowe706@gmail.com>
2018-10-16 11:52:58 -0700

    Product model callbacks

--- app/models/product.rb
+++ app/models/product.rb
@@ -14,4 +14,27 @@ class Product < ApplicationRecord
     length: { minimum: 10 }
   }
 
+  # Callback methods
+  before_validation :set_defaults
+  before_save :capitalize_title
+
+  private
+
+  def set_defaults
+    # We could also set this default at the database level in our migration.
+    # It really depends on the structure of your models and specific use case
+    # in terms of which is better.
+    self.price ||= 1
+  end
+
+  def capitalize_title
+    # We definitely need self.title on the left hand side as otherwise it will
+    # just set a local variable title. For consistency we also use it on the
+    # right hand side. But if we don't have it Rails will first look for a
+    # local variable title in the capitalize_title method. When it doesn't find
+    # that, it will go up a level (into the Product class) and if you have an
+    # attribute 'title' on your Product object it will use that.
+    self.title = self.title.capitalize
+  end
+
 end
